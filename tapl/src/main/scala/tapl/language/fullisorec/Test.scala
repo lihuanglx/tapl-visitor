091aa5cdf623b08c250a543eb4ad35e08f83abc0
Huang Li <lihuanglx@gmail.com>
2017-06-12 14:36:01 +0800

    Fullisorec typer

---
+++ tapl/src/main/scala/tapl/language/fullisorec/Test.scala
@@ -0,0 +1,24 @@
+package tapl.language.fullisorec
+
+import tapl.common._
+
+object Test {
+  val parser = new Parse[Alg, TAlg] {}
+
+  def main(args: Array[String]): Unit = {
+    val input = "\\x: (Rec A.A->$T). unfold [Rec A.A->$T] x"
+    val ast: TExp[Alg, Exp[TAlg]] = parser.parse(input).get
+    go(ast, 1)
+  }
+
+  def go(e: TExp[Alg, Exp[TAlg]], step: Int): Unit = {
+    println("Step " + step.toString + ": ")
+    println("  Term: " + e(Print))
+    println("  Type: " + e(Typer)(Context.empty())(TPrint))
+    if (e(IsVal)) {
+      println("Value")
+    } else {
+      go(e(Eval), step + 1)
+    }
+  }
+}
--- tapl/src/main/scala/tapl/language/fullisorec/Typer.scala
+++ tapl/src/main/scala/tapl/language/fullisorec/Typer.scala
@@ -3,19 +3,55 @@ package tapl.language.fullisorec
 import tapl.common._
 import tapl.component.rectype
 import tapl.language.fullsimple
+import tapl.language.fullisorec.TAlg.Factory._
 
 trait Typer[A[-R, E, -F] <: Alg[R, E, F], B[-X, Y] <: TAlg[X, Y]]
-  extends Alg[TExp[A, Exp[B]], Type[B], Exp[B]] with fullsimple.Typer[A, B] {
+  extends Alg[TExp[A, Exp[B]], Type[B], Exp[B]] with fullsimple.Typer[A, B] with ISubst[B] {
 
-  override def tmFold(e: TExp[A, Exp[B]], t: Exp[B]): Type[B] = ???
+  override def tmFold(e: TExp[A, Exp[B]], t: Exp[B]): Type[B] = c => t match {
+    case TyRec(x, u) if tEquals(apply(e)(c))(subst(x, t)(u)) => t
+    case _ => typeError()
+  }
 
-  override def tmUnfold(e: TExp[A, Exp[B]], t: Exp[B]): Type[B] = ???
+  override def tmUnfold(e: TExp[A, Exp[B]], t: Exp[B]): Type[B] = c => t match {
+    case TyRec(x, u) if tEquals(apply(e)(c))(t) => subst(x, t)(u)
+    case _ => typeError()
+  }
 }
 
 object Typer extends Typer[Alg, TAlg] with Impl[Type[TAlg]] {
-  override val tEquals: Exp[TAlg] => Exp[TAlg] => Boolean = ???
+  override val tEquals: Exp[TAlg] => Exp[TAlg] => Boolean = _ (TEquals)(Set.empty)
+
+  override val subst: (String, Exp[TAlg]) => TAlg[Exp[TAlg], Exp[TAlg]] =
+    (x, e) => new TSubstImpl(x, e)
 }
 
-trait TEquals[A[-X, Y] <: TAlg[X, Y]] extends TAlg[Exp[A], Exp[A] => Boolean] with fullsimple.TEquals[A] {
+trait TEquals[A[-X, Y] <: TAlg[X, Y]] extends TAlg[Exp[A], Set[(String, String)] => Exp[A] => Boolean]
+  with fullsimple.TAlg.Lifter[Exp[A], Exp[A] => Boolean, Set[(String, String)]] with ISubst[A] {
 
+  override def go(c: Set[(String, String)]) = new fullsimple.TEquals[A] {
+    override def apply(t: Exp[A]): (Exp[A]) => Boolean = TEquals.this.apply(t)(c)
+  }
+
+  override def tyRec(x: String, t: Exp[A]): Set[(String, String)] => Exp[A] => Boolean = c => {
+    case TyRec(x2, t2) => apply(t)(c + (x -> x2))(t2)
+    case _ => false
+  }
+
+  override def tyVar(x: String): Set[(String, String)] => Exp[A] => Boolean = c => {
+    case TyVar(y) => c((x, y))
+    case _ => false
+  }
+}
+
+object TEquals extends TEquals[TAlg] with TImpl[Set[(String, String)] => Exp[TAlg] => Boolean] {
+  override val subst: (String, Exp[TAlg]) => TAlg[Exp[TAlg], Exp[TAlg]] =
+    (x, e) => new TSubstImpl(x, e)
+}
+
+trait TSubst[A[-X, Y] <: TAlg[X, Y]] extends TAlg.Transform[A] with rectype.TSubst[A]
+
+class TSubstImpl(_x: String, _e: Exp[TAlg]) extends TSubst[TAlg] with TImpl[Exp[TAlg]] {
+  override val x: String = _x
+  override val e: Exp[TAlg] = _e
 }
