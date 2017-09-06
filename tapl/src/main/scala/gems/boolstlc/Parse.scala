41937b3c7e77994bc894a4c74e004923fbdcc109
Huang Li <lihuanglx@gmail.com>
2017-09-06 13:16:35 +0800

    Example

---
+++ tapl/src/main/scala/gems/boolstlc/Parse.scala
@@ -0,0 +1,14 @@
+package gems.boolstlc
+
+import gems.common._
+import gems.{bool, stlc}
+
+trait Parse[A[-R, E, -F] <: Term[R, E, F], B[-X, Y] <: Type[X, Y]]
+  extends stlc.Parse[A, B] with bool.Parse[A[-?, ?, Exp[B]], B] {
+
+  lazy val pBoolstlcE: PackratParser[Exp2[A, Exp[B]]] = pBoolE ||| pStlcE
+  lazy val pBoolstlcT: PackratParser[Exp[B]] = pBoolT ||| pStlcT
+
+  override lazy val pE: PackratParser[Exp2[A, Exp[B]]] = pBoolstlcE
+  override lazy val pT: PackratParser[Exp[B]] = pBoolstlcT
+}
--- tapl/src/main/scala/gems/boolstlc/Term.scala
+++ tapl/src/main/scala/gems/boolstlc/Term.scala
@@ -6,3 +6,6 @@ import gems.{bool, stlc}
 
 @Lang("boolstlc")
 trait Term[-R, E, -T] extends bool.Term[R, E] with stlc.Term[R, E, T]
+
+@Lang("boolstlc")
+trait Type[-R, E] extends bool.Type[R, E] with stlc.Type[R, E]
