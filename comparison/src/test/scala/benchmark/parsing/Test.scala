6f08198d56dbad98da933feba9ee950337156075
Huang Li <lihuanglx@gmail.com>
2017-09-08 17:25:57 +0800

    Add code of OA

--- comparison/src/test/scala/benchmark/parsing/Test.scala
+++ comparison/src/test/scala/benchmark/parsing/Test.scala
@@ -38,74 +38,74 @@ class Test extends FunSuite {
   }
 
   test("arith") {
-    compare("arith", visitor.arith.Test.benchmarkParsing, comp.parsing.arith.Parser.input)
+    compare("arith", visitor.arith.Test.benchmarkParsing, comp.parsing.nonmod.arith.Parser.input)
   }
 
   test("untyped") {
-    compare("untyped", visitor.untyped.Test.benchmarkParsing, comp.parsing.untyped.Parser.input)
+    compare("untyped", visitor.untyped.Test.benchmarkParsing, comp.parsing.nonmod.untyped.Parser.input)
   }
 
   test("fulluntyped") {
-    compare("fulluntyped", visitor.fulluntyped.Test.benchmarkParsing, comp.parsing.fulluntyped.Parser.input)
+    compare("fulluntyped", visitor.fulluntyped.Test.benchmarkParsing, comp.parsing.nonmod.fulluntyped.Parser.input)
   }
 
   test("tyarith") {
-    compare("tyarith", visitor.tyarith.Test.benchmarkParsing, comp.parsing.tyarith.Parser.input)
+    compare("tyarith", visitor.tyarith.Test.benchmarkParsing, comp.parsing.nonmod.tyarith.Parser.input)
   }
 
   test("simplebool") {
-    compare("simplebool", visitor.simplebool.Test.benchmarkParsing, comp.parsing.simplebool.Parser.input)
+    compare("simplebool", visitor.simplebool.Test.benchmarkParsing, comp.parsing.nonmod.simplebool.Parser.input)
   }
 
   test("fullsimple") {
-    compare("fullsimple", visitor.fullsimple.Test.benchmarkParsing, comp.parsing.fullsimple.Parser.input)
+    compare("fullsimple", visitor.fullsimple.Test.benchmarkParsing, comp.parsing.nonmod.fullsimple.Parser.input)
   }
 
   test("bot") {
-    compare("bot", visitor.bot.Test.benchmarkParsing, comp.parsing.bot.Parser.input)
+    compare("bot", visitor.bot.Test.benchmarkParsing, comp.parsing.nonmod.bot.Parser.input)
   }
 
   test("fullerror") {
-    compare("fullerror", visitor.fullerror.Test.benchmarkParsing, comp.parsing.fullerror.Parser.input)
+    compare("fullerror", visitor.fullerror.Test.benchmarkParsing, comp.parsing.nonmod.fullerror.Parser.input)
   }
 
   test("rcdsubbot") {
-    compare("rcdsubbot", visitor.rcdsubbot.Test.benchmarkParsing, comp.parsing.rcdsubbot.Parser.input)
+    compare("rcdsubbot", visitor.rcdsubbot.Test.benchmarkParsing, comp.parsing.nonmod.rcdsubbot.Parser.input)
   }
 
   test("fullsub") {
-    compare("fullsub", visitor.fullsub.Test.benchmarkParsing, comp.parsing.fullsub.Parser.input)
+    compare("fullsub", visitor.fullsub.Test.benchmarkParsing, comp.parsing.nonmod.fullsub.Parser.input)
   }
 
   test("fullref") {
-    compare("fullref", visitor.fullref.Test.benchmarkParsing, comp.parsing.fullref.Parser.input)
+    compare("fullref", visitor.fullref.Test.benchmarkParsing, comp.parsing.nonmod.fullref.Parser.input)
   }
 
   test("equirec") {
-    compare("equirec", visitor.equirec.Test.benchmarkParsing, comp.parsing.equirec.Parser.input)
+    compare("equirec", visitor.equirec.Test.benchmarkParsing, comp.parsing.nonmod.equirec.Parser.input)
   }
 
   test("fullequirec") {
-    compare("fullequirec", visitor.fullequirec.Test.benchmarkParsing, comp.parsing.fullequirec.Parser.input)
+    compare("fullequirec", visitor.fullequirec.Test.benchmarkParsing, comp.parsing.nonmod.fullequirec.Parser.input)
   }
 
   test("fullisorec") {
-    compare("fullisorec", visitor.fullisorec.Test.benchmarkParsing, comp.parsing.fullisorec.Parser.input)
+    compare("fullisorec", visitor.fullisorec.Test.benchmarkParsing, comp.parsing.nonmod.fullisorec.Parser.input)
   }
 
   test("recon") {
-    compare("recon", visitor.recon.Test.benchmarkParsing, comp.parsing.recon.Parser.input)
+    compare("recon", visitor.recon.Test.benchmarkParsing, comp.parsing.nonmod.recon.Parser.input)
   }
 
   test("fullrecon") {
-    compare("fullrecon", visitor.fullrecon.Test.benchmarkParsing, comp.parsing.fullrecon.Parser.input)
+    compare("fullrecon", visitor.fullrecon.Test.benchmarkParsing, comp.parsing.nonmod.fullrecon.Parser.input)
   }
 
   test("fullpoly") {
-    compare("fullpoly", visitor.fullpoly.Test.benchmarkParsing, comp.parsing.fullpoly.Parser.input)
+    compare("fullpoly", visitor.fullpoly.Test.benchmarkParsing, comp.parsing.nonmod.fullpoly.Parser.input)
   }
 
   test("fullomega") {
-    compare("fullomega", visitor.fullomega.Test.benchmarkParsing, comp.parsing.fullomega.Parser.input)
+    compare("fullomega", visitor.fullomega.Test.benchmarkParsing, comp.parsing.nonmod.fullomega.Parser.input)
   }
 }
